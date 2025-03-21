7ddd04629c5401c5450d1b514f2b65b16a88e396
LNUitTutor <lnu.it.tutor@gmail.com>
2025-03-21 09:40:35 +0200

    Unit tests addaed

---
+++ UnitTests/UnitTests.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\ClassesHierarchy\Classes.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTests
+{
+	TEST_CLASS(TestBase)
+	{
+	public:
+		
+		TEST_METHOD(TestParamCreation)
+		{
+			Base a(5);
+			Assert::AreEqual(5, a.getA());
+		}
+		TEST_METHOD(TestDefaultCreation)
+		{
+			Base a;
+			Assert::AreEqual(0, a.getA());
+		}
+	};
+}
